af45e59717f002ada8f253f3fe97b5b46ced447d
Esmail Essam <esmailessam6@gmail.com>
2024-05-31 10:05:37 +0300

    Add the complete project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nyc-duration-prediction
+
+To build a docker image
+```sh
+docker build -t duration-prediction .
+```
+
+To run a docker image
+```sh
+docker run -p 5000:5000 duration-prediction
+```
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+app = Flask(__name__)
+
+# Load model
+duration_model = pickle.load(open("Models/duration-prediction.sav", "rb"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["GET", "POST"])
+def predict():
+
+    passenger_count = float(request.form["passenger_count"])
+    trip_distance = float(request.form["trip_distance"])
+    PULocationID = float(request.form["pull_location_id"])
+    DOLocationID = float(request.form["down_location_id"])
+    total_amount = float(request.form["total_amount"])
+
+    test_input = [
+        [passenger_count, trip_distance, PULocationID, DOLocationID, total_amount]
+    ]
+
+    prediction = round(duration_model.predict(test_input)[0], 2)
+
+    return render_template("index.html", prediction=prediction)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
